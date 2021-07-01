07b3acaf04572721721bb2a04041872bc9eba646
clyesonbarros <cleysonbarros@yahoo.com.br>
2021-07-01 16:31:18 -0300

    commit

--- src/test/java/com/marketplace/produto/service/CategoriaServiceTest.java
+++ src/test/java/com/marketplace/produto/service/CategoriaServiceTest.java
@@ -13,6 +13,8 @@ import org.mockito.Mockito;
 import org.modelmapper.ModelMapper;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 import static org.mockito.ArgumentMatchers.any;
@@ -30,7 +32,7 @@ public class CategoriaServiceTest {
     private CategoriaRepository categoriaRepository;
 
     @Test
-    public void deveBuscarCategorias(){
+    public void deveBuscarCategoria(){
         Long id = 1L;
         CategoriaDTO categoriaDTO = getCategoriaDTO();
 
@@ -54,6 +56,65 @@ public class CategoriaServiceTest {
         } catch (RegistroNaoEncontradoException e) {
             Assertions.assertEquals("categoria nao encontrada", e.getMessage());
         }
+
+    }
+    @Test
+    public void listarCategoria(){
+
+        List<Categoria> categorias = new ArrayList<>();
+        Categoria categoria = new Categoria();
+        categorias.add(categoria);
+        categoria.setDescricao("Material Escolar");
+
+        Categoria categoria1 = new Categoria();
+        categorias.add(categoria1);
+        categoria1.setDescricao("Acessorios de Informatica");
+
+        Mockito.when(categoriaRepository.findAll()).thenReturn(categorias);
+        Mockito.when(modelMapper.map(any(),any())).thenReturn(getCategoriaDTO());
+
+
+        List<CategoriaDTO> categoriaDto = categoriaService.list();
+
+        Assertions.assertEquals(2,categoriaDto.size());
+
+    }
+    @Test
+    public void salvarCategoria(){
+
+        CategoriaDTO categoriaDTO = getCategoriaDTO();
+        categoriaDTO.setDescricao("Material Escolar");
+        Categoria categoria =categoriaService.salvar(categoriaDTO);
+
+        Mockito.when(categoriaRepository.save(categoria)).thenReturn(categoria);
+        Mockito.when(modelMapper.map(any(),any())).thenReturn(getCategoriaDTO());
+
+        Assertions.assertEquals("Material Escolar",categoriaDTO.getDescricao());
+
+    }
+
+    @Test
+    public void AtulizaCategoria(){ ;
+         categoriaService.atualizarCategoria(getCategoriaDTO());
+         Categoria categoria= new Categoria();
+         categoria.setDescricao("Material Escolar");
+
+         Mockito.when(categoriaRepository.save(categoria)).thenReturn(categoria);
+         Mockito.when(modelMapper.map(any(),any())).thenReturn(getCategoriaDTO());
+
+         Assertions.assertEquals("Material Escolar",categoria.getDescricao());
+
+
+    }
+
+    @Test
+    public void deletar() {
+     categoriaService.remover(1L);
+     Optional<Categoria> categoria = categoriaRepository.findById(1L);
+
+     Assertions.assertFalse(categoria.isPresent());
+
+
 
     }
 
