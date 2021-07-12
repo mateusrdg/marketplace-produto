45da439970a4404d2d8467f02e7531466c9358d7
clyesonbarros <cleysonbarros@yahoo.com.br>
2021-07-12 15:39:10 -0300

    commit

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -9,6 +9,8 @@ insert into produto_categorias(produto_id,categoria_id)values(9,2);
 insert into venda(id,nome)values(1,'jose');
 insert into venda(id,nome)values(2,'antonio');
 
-insert into item(id,produto,quantidade,id_venda)values(1,'Teclado',2,1);
-insert into item(id,produto,quantidade,id_venda)values(2,'Lapis',1,2);
-insert into item(id,preco ,produto ,quantidade,id_venda)values(4,1,'Borracha',2,2);
+insert into item(id,preco,produto,quantidade,id_venda)values(1,15,'Teclado',2,1);
+insert into item(id,preco,produto,quantidade,id_venda)values(2,1,'Lapis',1,2);
+insert into item(id,preco,produto,quantidade,id_venda)values(3,15,'Teclado',2,1);
+insert into item(id,preco,produto ,quantidade,id_venda)values(4,1,'Borracha',2,2);
+insert into item(id,preco,produto ,quantidade,id_venda)values(5,10,'caderno',1,2);
